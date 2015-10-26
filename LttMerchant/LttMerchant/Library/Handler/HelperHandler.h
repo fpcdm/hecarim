c0c5eba381a22bb70461ce1ad847f62f77f52076
yanghf <775317846@qq.com>
2015-10-26 18:05:41 +0800

    重置密码方法

---
+++ LttMerchant/LttMerchant/Library/Handler/HelperHandler.h
@@ -0,0 +1,51 @@
+//
+//  HelperHandler.h
+//  LttCustomer
+//
+//  Created by wuyong on 15/6/26.
+//  Copyright (c) 2015年 Gilbert. All rights reserved.
+//
+
+#import "BaseHandler.h"
+
+@interface HelperHandler : BaseHandler
+
+/**
+ *  检查手机号是否已注册
+ *
+ *  @param mobile 手机号
+ *  @param success 成功回调
+ *  @param failure 失败回调
+ */
+- (void) checkMobile: (NSString *) mobile success: (SuccessBlock) success failure: (FailedBlock) failure;
+
+/**
+ *  发送验证码
+ *
+ *  @param mobile 手机号
+ *  @param success 成功回调
+ *  @param failure 失败回调
+ */
+- (void) sendMobileCode: (NSString *) mobile success: (SuccessBlock) success failure: (FailedBlock) failure;
+
+/**
+ *  验证短信验证码
+ *
+ *  @param mobile 手机号
+ *  @param code 验证码
+ *  @param success 成功回调
+ *  @param failure 失败回调
+ */
+- (void) verifyMobileCode: (NSString *) mobile code: (NSString *) code success: (SuccessBlock) success failure: (FailedBlock) failure;
+
+/**
+ *  重置密码
+ *
+ *  @param mobile 手机号
+ *  @param vCode 安全码
+ *  @param success 成功回调
+ *  @param failure 失败回调
+ */
+- (void) resetPassword: (NSString *) mobile vCode: (NSString *) vCode password: (NSString *) password success: (SuccessBlock) success failure: (FailedBlock) failure;
+
+@end
---
+++ LttMerchant/LttMerchant/Library/Handler/HelperHandler.m
@@ -0,0 +1,74 @@
+//
+//  HelperHandler.m
+//  LttCustomer
+//
+//  Created by wuyong on 15/6/26.
+//  Copyright (c) 2015年 Gilbert. All rights reserved.
+//
+
+#import "HelperHandler.h"
+
+@implementation HelperHandler
+
+//检查手机号是否存在
+- (void) checkMobile:(NSString *)mobile success:(SuccessBlock)success failure:(FailedBlock)failure {
+    RestKitUtil *sharedClient = [RestKitUtil sharedClient];
+    RKResponseDescriptor *responseDescriptor = [sharedClient addResponseDescriptor:[ResultEntity class] mappingParam:@{@"result": @"data"}];
+    
+    NSString *restPath = [NSString stringWithFormat:@"user/mobilecheck/%@", mobile];
+    [sharedClient getObject:[ResultEntity new] path:restPath param:nil success:^(NSArray *result){
+        [sharedClient removeResponseDescriptor:responseDescriptor];
+        
+        success(result);
+    } failure:^(ErrorEntity *error){
+        [sharedClient removeResponseDescriptor:responseDescriptor];
+        failure(error);
+    }];
+}
+
+//发送校验码
+- (void) sendMobileCode:(NSString *)mobile success:(SuccessBlock)success failure:(FailedBlock)failure
+{
+    RestKitUtil *sharedClient = [RestKitUtil sharedClient];
+    
+    NSString *restPath = [NSString stringWithFormat:@"sendSmsCode/%@", mobile];
+    [sharedClient getObject:[ResultEntity new] path:restPath param:nil success:^(NSArray *result){
+        success(result);
+    } failure:^(ErrorEntity *error){
+        failure(error);
+    }];
+}
+
+//验证校验码
+- (void) verifyMobileCode:(NSString *)mobile code:(NSString *)code success:(SuccessBlock)success failure:(FailedBlock)failure
+{
+    RestKitUtil *sharedClient = [RestKitUtil sharedClient];
+    RKResponseDescriptor *responseDescriptor = [sharedClient addResponseDescriptor:[ResultEntity class] mappingParam:@{@"vCode": @"data"}];
+    
+    NSDictionary *param = @{@"code": (code ? code : @"")};
+    
+    NSString *restPath = [NSString stringWithFormat:@"verifySmsCode/%@", mobile];
+    [sharedClient postObject:[ResultEntity new] path:restPath param:param success:^(NSArray *result){
+        [sharedClient removeResponseDescriptor:responseDescriptor];
+        success(result);
+    } failure:^(ErrorEntity *error){
+        [sharedClient removeResponseDescriptor:responseDescriptor];
+        failure(error);
+    }];
+}
+
+//重置密码
+- (void) resetPassword:(NSString *)mobile vCode:(NSString *)vCode password:(NSString *)password success:(SuccessBlock)success failure:(FailedBlock)failure
+{
+    RestKitUtil *sharedClient = [RestKitUtil sharedClient];
+    NSDictionary *param = @{@"newPass":(password ? password : @""),@"vCode":(vCode ? vCode : @"")};
+    
+    NSString * restPath = [NSString stringWithFormat:@"user/password/%@",mobile];
+    [sharedClient postObject:[RestKitUtil new] path:restPath param:param success:^(NSArray *result) {
+        success(result);
+    } failure:^(ErrorEntity *error) {
+        failure(error);
+    }];
+}
+
+@end
