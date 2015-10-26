cb9165be2ec845ee079a660c078a4a3a66ef8395
yanghf <775317846@qq.com>
2015-10-26 18:04:54 +0800

    忘记密码视图

---
+++ LttMerchant/LttMerchant/Controller/Home/ForgetPasswordViewController.h
@@ -0,0 +1,14 @@
+//
+//  ForgetPasswordViewController.h
+//  LttMerchant
+//
+//  Created by 杨海锋 on 15/10/19.
+//  Copyright © 2015年 Gilbert. All rights reserved.
+//
+
+#import "AppActivity.h"
+
+@interface ForgetPasswordViewController : AppActivity
+
+
+@end
