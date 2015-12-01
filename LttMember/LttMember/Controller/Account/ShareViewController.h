3bbac20ac5186242d103b9b0ccd1ff34f965449c
lszzy <lingshizhuangzi@gmail.com>
2015-12-01 12:32:36 +0800

    测试冲突

---
+++ LttMember/LttMember/Controller/Account/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  LttMember
+//
+//  Created by wuyong on 15/12/1.
+//  Copyright © 2015年 Gilbert. All rights reserved.
+//
+
+#import "AppUserViewController.h"
+
+@interface ShareViewController : AppUserViewController
+
+@end
