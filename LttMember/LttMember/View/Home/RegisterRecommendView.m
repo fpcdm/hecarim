31e1f99c9f507685514c23f25d93400c33d62e7a
yanghf <775317846@qq.com>
2015-12-01 17:22:13 +0800

    一个用户只能填写一次推荐人

--- LttMember/LttMember/View/Home/RegisterRecommendView.m
+++ LttMember/LttMember/View/Home/RegisterRecommendView.m
@@ -80,7 +80,7 @@
     //提示
     UILabel *tipLabel = [[UILabel alloc] init];
     tipLabel.text = @"提示:";
-    NSString *labelString = @"提示:\n1.一台手机只能填写一次。\n2.可跳过此步骤，以后你可以在【账户】\n-【推荐与分享】中补充填写推荐人。";
+    NSString *labelString = @"提示:\n1.一个用户只能填写一次推荐人。\n2.可跳过此步骤，以后你可以在【账户】\n-【推荐与分享】中补充填写推荐人。";
     tipLabel.textColor = COLOR_MAIN_BLACK;
     tipLabel.font = FONT_MAIN;
     tipLabel.numberOfLines = 4;
