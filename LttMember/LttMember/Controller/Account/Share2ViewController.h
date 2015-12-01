f33fa453282b63e55f743f2eeb03d9cf8fe00a5d
lszzy <lingshizhuangzi@gmail.com>
2015-12-01 12:44:33 +0800

    测试冲突

---
+++ LttMember/LttMember/Controller/Account/Share2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Share2ViewController.h
+//  LttMember
+//
+//  Created by wuyong on 15/12/1.
+//  Copyright © 2015年 Gilbert. All rights reserved.
+//
+
+#import "AppUserViewController.h"
+
+@interface Share2ViewController : AppUserViewController
+
+@end
