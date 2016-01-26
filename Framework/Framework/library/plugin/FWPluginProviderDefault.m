dc3b1599c7a31b21883388dabb84f18b8f5de2b4
lszzy <admin@ocphp.com>
2016-01-26 09:31:46 +0800

    修正插件bug

--- Framework/Framework/library/plugin/FWPluginProviderDefault.m
+++ Framework/Framework/library/plugin/FWPluginProviderDefault.m
@@ -10,13 +10,30 @@
 #import "FWPluginDialogDefault.h"
 
 @implementation FWPluginProviderDefault
+{
+    //对象缓存池
+    NSMutableDictionary *plugins;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        plugins = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
 
 - (id)providePlugin:(NSString *)name
 {
     id plugin = nil;
     //默认弹出框
     if ([FWPluginDialogName isEqualToString:name]) {
-        plugin = [[FWPluginDialogDefault alloc] init];
+        plugin = [plugins objectForKey:name];
+        if (!plugin) {
+            plugin = [[FWPluginDialogDefault alloc] init];
+            [plugins setObject:plugin forKey:name];
+        }
     }
     return plugin;
 }
