a0c79667d62430cf047118ecea2385c2df06b139
lszzy <admin@ocphp.com>
2016-01-26 17:43:28 +0800

    增加Loading插件

---
+++ Framework/Framework/library/plugin/FWPluginLoadingDefault.h
@@ -0,0 +1,14 @@
+//
+//  FWPluginLoadingDefault.h
+//  Framework
+//
+//  Created by wuyong on 16/1/26.
+//  Copyright © 2016年 ocphp.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FWPluginLoading.h"
+
+@interface FWPluginLoadingDefault : NSObject<FWPluginLoading>
+
+@end
---
+++ Framework/Framework/library/plugin/FWPluginLoadingDefault.m
@@ -0,0 +1,13 @@
+//
+//  FWPluginLoadingDefault.m
+//  Framework
+//
+//  Created by wuyong on 16/1/26.
+//  Copyright © 2016年 ocphp.com. All rights reserved.
+//
+
+#import "FWPluginLoadingDefault.h"
+
+@implementation FWPluginLoadingDefault
+
+@end
