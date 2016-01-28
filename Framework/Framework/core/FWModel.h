7449d49139c56b411617c5a8b209d300ffba8a53
lszzy <admin@ocphp.com>
2016-01-28 11:52:40 +0800

    增加设计类库

---
+++ Framework/Framework/core/FWModel.h
@@ -0,0 +1,14 @@
+//
+//  FWModel.h
+//  Framework
+//
+//  Created by wuyong on 16/1/28.
+//  Copyright © 2016年 ocphp.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//主要是数据模型，要不要分离Entity
+@interface FWModel : NSObject
+
+@end
