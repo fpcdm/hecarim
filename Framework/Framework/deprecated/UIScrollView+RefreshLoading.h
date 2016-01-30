00bbed1b590bc26050d4da643ccebc5691e23c93
吴勇 <admin@ocphp.com>
2016-01-30 18:01:38 +0800

    优化框架

--- Framework/Framework/deprecated/UIScrollView+RefreshLoading.h
+++ Framework/Framework/deprecated/UIScrollView+RefreshLoading.h
@@ -7,39 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
-
-//刷新加载结果
-typedef enum {
-    //一条数据都没有
-    RefreshLoadingStateNoData = 1,
-    //数据未加载完毕
-    RefreshLoadingStateMoreData,
-    //全部加载完毕，没有更多数据
-    RefreshLoadingStateNoMoreData
-} RefreshLoadingState;
+#import "UIScrollView+Framework.h"
 
 //定义刷新分类
 @interface UIScrollView (RefreshLoading)
-
-//空数据视图，不设置和没有更多效果一致
-@property (strong, nonatomic) UIView *emptyView;
-
-//下拉刷新
-- (void) setRefreshingHeader: (id) target action: (SEL) action;
-
-//上拉加载
-- (void) setLoadingFooter: (id) target action: (SEL) action;
-
-//开始刷新
-- (void) startRefreshing;
-
-//开始加载更多
-- (void) startLoading;
-
-//停止刷新加载
-- (void) stopRefreshLoading;
-
-//修改状态
-- (void) setRefreshLoadingState:(RefreshLoadingState) state;
 
 @end
