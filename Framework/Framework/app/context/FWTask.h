1369ca50c43c52583ed49a95136d5be71e7162c4
lszzy <admin@ocphp.com>
2016-03-10 14:39:59 +0800

    完成FWTask

--- Framework/Framework/app/context/FWTask.h
+++ Framework/Framework/app/context/FWTask.h
@@ -8,6 +8,37 @@
 
 #import <Foundation/Foundation.h>
 
-@interface FWTask : NSObject
+//任务基类
+@interface FWTask : NSOperation
+
+@prop_strong(NSError *, error)
+
+//子类重写，任务执行完成，需调用finishWithError:
+- (void)executeTask;
+
+//标记任务完成，error为空表示任务成功
+- (void)finishWithError:(NSError *)error;
+
+//是否需要主线程执行，会阻碍UI渲染，默认NO
+- (BOOL)needMainThread;
+
+@end
+
+//任务管理器，兼容NSBlockOperation和NSInvocationOperation
+@interface FWTaskManager : NSObject
+
+@singleton(FWTaskManager)
+
+//并发操作的最大任务数
+@prop_assign(NSInteger, maxConcurrentTaskCount)
+
+//从配置数组添加任务
+- (void)addTaskConfig:(NSArray<NSDictionary *> *)config;
+
+//添加单个任务
+- (void)addTask:(NSOperation *)task;
+
+//批量添加任务
+- (void)addTasks:(NSArray<NSOperation *> *)tasks;
 
 @end
--- Framework/Framework/app/context/FWTask.m
+++ Framework/Framework/app/context/FWTask.m
@@ -8,6 +8,290 @@
 
 #import "FWTask.h"
 
+typedef NS_ENUM(NSInteger, FWTaskState) {
+    FWTaskStateCreate,
+    FWTaskStateReady = 1,
+    FWTaskStateLoading,
+    FWTaskStateSuccessed,
+    FWTaskStateFailure,
+    FWTaskStateCanceled,
+};
+
+@interface FWTask ()
+
+@property (nonatomic, assign) FWTaskState state;
+@property (nonatomic, strong, readonly) NSRecursiveLock *lock;
+
+@end
+
 @implementation FWTask
+
+@synthesize lock = _lock;
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.state = FWTaskStateReady;
+    }
+    return self;
+}
+
+- (NSRecursiveLock *)lock
+{
+    if (!_lock) {
+        _lock = [[NSRecursiveLock alloc] init];
+    }
+    return _lock;
+}
+
+- (void)start
+{
+    [self.lock lock];
+    if ([self isReady]) {
+        self.state = FWTaskStateLoading;
+        [FWLog debug:@"task %@ start", [self.class description]];
+        [self.lock unlock];
+        
+        if ([self needMainThread]) {
+            if ([NSThread isMainThread]) {
+                [self executeTask];
+            } else {
+                [self performSelectorOnMainThread:@selector(executeTask) withObject:nil waitUntilDone:NO];
+            }
+        } else {
+            [self executeTask];
+        }
+    } else {
+        [self.lock unlock];
+    }
+}
+
+- (void)executeTask
+{
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:[NSString stringWithFormat:@"task %@ must override executeTask", [self.class description]]
+                                 userInfo:nil];
+}
+
+- (void)finishWithError:(NSError *)error
+{
+    [self.lock lock];
+    if (![self isFinished]) {
+        [FWLog debug:@"task %@ finish", [self.class description]];
+        if (error) {
+            self.error = error;
+            self.state = FWTaskStateFailure;
+        } else {
+            self.state = FWTaskStateSuccessed;
+        }
+    }
+    [self.lock unlock];
+}
+
+- (BOOL)needMainThread
+{
+    return NO;
+}
+
+- (void)cancel
+{
+    [self.lock lock];
+    
+    if (![self isFinished]) {
+        self.state = FWTaskStateCanceled;
+        [super cancel];
+        [FWLog debug:@"task %@ cancel", [self.class description]];
+    }
+    
+    [self.lock unlock];
+}
+
+//isConcurrent < iOS7
+- (BOOL)isAsynchronous
+{
+    return YES;
+}
+
+- (BOOL)isReady
+{
+    return self.state == FWTaskStateReady && [super isReady];
+}
+
+- (BOOL)isFinished
+{
+    return self.state == FWTaskStateSuccessed || self.state == FWTaskStateFailure || self.state == FWTaskStateCanceled;
+}
+
+- (BOOL)isExecuting
+{
+    return self.state == FWTaskStateLoading;
+}
+
+- (BOOL)isValidTransition:(FWTaskState)fromState toState:(FWTaskState)toState
+{
+    switch (fromState) {
+        case FWTaskStateReady:
+        {
+            switch (toState) {
+                case FWTaskStateLoading:
+                case FWTaskStateSuccessed:
+                case FWTaskStateFailure:
+                case FWTaskStateCanceled:
+                    return YES;
+                    break;
+                default:
+                    return NO;
+                    break;
+            }
+            break;
+        }
+        case FWTaskStateLoading:
+        {
+            switch (toState) {
+                case FWTaskStateSuccessed:
+                case FWTaskStateFailure:
+                case FWTaskStateCanceled:
+                    return YES;
+                    break;
+                default:
+                    return NO;
+                    break;
+            }
+        }
+        case (FWTaskState)0:
+        {
+            if (toState == FWTaskStateReady) {
+                return YES;
+            } else {
+                return NO;
+            }
+        }
+        default:
+            return NO;
+            break;
+    }
+}
+
+- (void)setState:(FWTaskState)state
+{
+    [self.lock lock];
+    if (![self isValidTransition:_state toState:state]) {
+        [self.lock unlock];
+        return;
+    }
+    
+    switch (state) {
+        case FWTaskStateCanceled:
+        {
+            [self willChangeValueForKey:@"isExecuting"];
+            [self willChangeValueForKey:@"isFinished"];
+            [self willChangeValueForKey:@"isCancelled"];
+            _state = state;
+            [self didChangeValueForKey:@"isExecuting"];
+            [self didChangeValueForKey:@"isFinished"];
+            [self didChangeValueForKey:@"isCancelled"];
+            break;
+        }
+        case FWTaskStateLoading:
+        {
+            [self willChangeValueForKey:@"isExecuting"];
+            _state = state;
+            [self didChangeValueForKey:@"isExecuting"];
+            break;
+        }
+        case FWTaskStateSuccessed:
+        case FWTaskStateFailure:
+        {
+            [self willChangeValueForKey:@"isFinished"];
+            [self willChangeValueForKey:@"isExecuting"];
+            _state = state;
+            [self didChangeValueForKey:@"isFinished"];
+            [self didChangeValueForKey:@"isExecuting"];
+            break;
+        }
+        case FWTaskStateReady:
+        {
+            [self willChangeValueForKey:@"isReady"];
+            _state = state;
+            [self didChangeValueForKey:@"isReady"];
+            break;
+        }
+        default:
+        {
+            _state = state;
+            break;
+        }
+    }
+    
+    [self.lock unlock];
+}
+
+@end
+
+#pragma mark -
+@implementation FWTaskManager
+{
+    NSOperationQueue *_taskQueue;
+}
+
+@def_singleton(FWTaskManager)
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _taskQueue = [[NSOperationQueue alloc] init];
+        _taskQueue.name = @"FWTaskManager.taskQueue";
+    }
+    return self;
+}
+
+- (void)setMaxConcurrentTaskCount:(NSInteger)maxConcurrentTaskCount
+{
+    _taskQueue.maxConcurrentOperationCount = maxConcurrentTaskCount;
+}
+
+- (NSInteger)maxConcurrentTaskCount
+{
+    return _taskQueue.maxConcurrentOperationCount;
+}
+
+- (void)addTaskConfig:(NSArray<NSDictionary *> *)config
+{
+    NSMutableDictionary *taskMap = [[NSMutableDictionary alloc] init];
+    
+    for (NSDictionary *taskInfo in config) {
+        //className
+        NSString *className = [taskInfo objectForKey:@"className"];
+        Class clazz = NSClassFromString(className);
+        if ([clazz isSubclassOfClass:[NSOperation class]]) {
+            NSOperation *task = [[clazz alloc] init];
+            [taskMap setObject:task forKey:className];
+            //dependency
+            NSArray *dependencyList = [[taskInfo objectForKey:@"dependency"] componentsSeparatedByString:@","];
+            if (dependencyList.count) {
+                for (NSString *depedencyClass in dependencyList) {
+                    NSOperation *preTask = [taskMap objectForKey:depedencyClass];
+                    if (preTask) [task addDependency:preTask];
+                }
+            }
+        }
+    }
+    
+    [self addTasks:[taskMap allValues]];
+}
+
+- (void)addTasks:(NSArray<NSOperation *> *)tasks
+{
+    if (tasks.count) {
+        [_taskQueue addOperations:tasks waitUntilFinished:NO];
+    }
+}
+
+- (void)addTask:(NSOperation *)task
+{
+    [_taskQueue addOperation:task];
+}
 
 @end
